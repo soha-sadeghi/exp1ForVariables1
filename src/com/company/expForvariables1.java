d87432334ed3ddbf53ff3f2f6feee18199775b7e
soha-sadeghi <97523339+soha-sadeghi@users.noreply.github.com>
2022-03-16 16:31:25 +0330

    feat:the code is endded .

--- src/com/company/expForvariables1.java
+++ src/com/company/expForvariables1.java
@@ -1,9 +1,1 @@
-package com.company;
-
-public class expForvariables1 {
-    public static void main(String[] args){
-        int a=1000;
-        float b=5.004f;
-        System.out.println(a+b);
-    }
-}
+package com.company;public class expForvariables1 { public static void main(String[] args){int a=1000;float b=5.004f;System.out.println(a+b);}}
