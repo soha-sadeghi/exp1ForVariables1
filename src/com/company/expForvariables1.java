ed57ee292e80e35680fd00a87031fb5601feef3c
soha-sadeghi <97523339+soha-sadeghi@users.noreply.github.com>
2022-03-16 16:35:38 +0330

    edited:the code is optimization .

--- src/com/company/expForvariables1.java
+++ src/com/company/expForvariables1.java
@@ -1,1 +1,7 @@
-package com.company;public class expForvariables1 { public static void main(String[] args){int a=1000;float b=5.004f;System.out.println(a+b);}}
+package com.company;
+public class expForvariables1 {
+    public static void main(String[]args){
+    int a=1000;float b=5.004f;
+    System.out.println(a+b);
+}
+}
